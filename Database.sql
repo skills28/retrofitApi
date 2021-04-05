42717953c7bbe54c16cb976d8fa49cd6e0e3997f
skills28 <80546161+skills28@users.noreply.github.com>
2021-04-05 14:47:57 +0500

    Add files via upload

---
+++ Database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `country_capital` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`country_name` TEXT NULL,
+	`capital_name` TEXT NULL,
+	PRIMARY KEY (`id`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=9
+;
